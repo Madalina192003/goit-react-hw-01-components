2f81cb48670f722052a4bf9e7b6dd65f19259db9
Madalina192003 <158836491+Madalina192003@users.noreply.github.com>
2024-10-06 21:24:15 +0300

    update app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './Profile';
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import Navigation from './Navigation';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    <BrowserRouter
+      basename={isProduction ? '/goit-react-hw-01-components' : ''}
     >
-      React homework template
-    </div>
+      <Navigation />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Profile
+              username={user.username}
+              tag={user.tag}
+              location={user.location}
+              avatar={user.avatar}
+              stats={user.stats}
+            />
+          }
+        />
+        <Route
+          path="/statistics"
+          element={<Statistics title="Upload stats" stats={data} />}
+        />
+        <Route path="/friends" element={<FriendList friends={friends} />} />
+        <Route
+          path="/transactions"
+          element={<TransactionHistory items={transactions} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
