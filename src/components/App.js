8102ea0af13a5315730fa981ea7be1b625aa6128
Madalina192003 <158836491+Madalina192003@users.noreply.github.com>
2024-10-06 20:10:44 +0300

    Creare exercitiul 1 profil user

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Profile from './Profile';
+import user from '../user.json';
+
+export const App = () => (
+  <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </div>
+);
